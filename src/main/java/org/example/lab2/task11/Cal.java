97f878b4c212c38d74e6f03d431c2e776c74bb0f
Oleg <olegkrygin9@gmail.com>
2023-03-27 21:32:01 +0300

    change task with calendar

--- src/main/java/org/example/lab2/task11/Cal.java
+++ src/main/java/org/example/lab2/task11/Cal.java
@@ -10,27 +10,32 @@ import static java.time.LocalDate.*;
 public class Cal {
 
     public static void print() {
-        printNamesOfWeekDays();
-        printNumbersOfMonth(of(now().getYear(), now().getMonthValue(), 1));
+        print(SUNDAY);
     }
 
-    public static void printNamesOfWeekDays() {
+    public static void print(DayOfWeek dayOfWeek) {
+
+        printNamesOfWeekDays(dayOfWeek.getValue());
+        printNumbersOfMonth(of(now().getYear(), now().getMonthValue(), 1), dayOfWeek);
+    }
+
+    public static void printNamesOfWeekDays(int delta) {
         for (int i = 0; i < 7; i++) {
-            out.printf("%4s", of((i + 6) % 7 + 1).toString().substring(0, 3));
+            out.printf("%4s", of((i + 6 + delta) % 7 + 1).toString().substring(0, 3));
         }
         out.println();
     }
 
-    public static void printNumbersOfMonth(LocalDate date) {
+    public static void printNumbersOfMonth(LocalDate date, DayOfWeek setDayOfWeek) {
         DayOfWeek weekday = date.getDayOfWeek();
-        int value = weekday.getValue()%7;
+        int value = (weekday.getValue()+7-setDayOfWeek.getValue())%7;
         for (int i = 1; i < value + 1; i++)
             out.print("    ");
 
         while (date.getMonthValue() == now().getMonthValue()) {
             out.printf("%4d", date.getDayOfMonth());
             date = date.plusDays(1);
-            if (date.getDayOfWeek() == SUNDAY) {
+            if (date.getDayOfWeek() == setDayOfWeek) {
                 out.println();
             }
         }

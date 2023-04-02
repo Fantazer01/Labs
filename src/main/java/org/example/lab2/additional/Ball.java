adfe9d12d980c50b5eb0daab9ce84ccdf91cd8e3
Oleg <olegkrygin9@gmail.com>
2023-04-02 23:15:36 +0300

    Провел некоторые изменения, касающиеся исключительно читаемости кода, по функционалу ничего не менял

--- src/main/java/org/example/lab2/additional/Ball.java
+++ src/main/java/org/example/lab2/additional/Ball.java
@@ -1,39 +1,49 @@
 package org.example.lab2.additional;
 
 public class Ball {
-    private static class Point{
-       private final int x;
+    public static class Point{
+        private final int x;
         private final int y;
         private final int z;
-        Point(){
+        public Point() {
             this.x=0;
             this.y=0;
             this.z=0;
         }
-        Point(int x,int y,int z){
+        public Point(int x,int y,int z) {
             this.x=x;
             this.y=y;
             this.z=z;
+        }
+
+        public double distance(Point a) {
+            return Math.sqrt(Math.pow((this.x-a.x),2)+Math.pow((this.y-a.y),2)+Math.pow((this.z-a.z),2));
         }
     }
     private final Point center;
     private final int radius;
     Ball(){
       this.center = new Point();
-      this.radius =0;
+      this.radius = 0;
     }
-    Ball(int x, int y, int z,int radius){
+    Ball(int x, int y, int z,int radius) {
         this.center = new Point(x,y,z);
-        this.radius =radius;
+        this.radius = radius;
     }
 
-    public double InerArea(Ball a){
-        double distance= Math.sqrt(Math.pow((this.center.x-a.center.x),2)+Math.pow((this.center.y-a.center.y),2)+Math.pow((this.center.z-a.center.z),2));
-        if(distance>=this.radius+a.radius)
+    public double distance(Ball a) {
+        return center.distance(a.center);
+    }
+
+    public double innerArea(Ball a) {
+        double distance = distance(a);
+
+        if (distance >= radius+a.radius)
             return 0;
-        else{
-            double Radius= Math.sqrt(Math.pow(this.radius,2) - Math.pow(((double) (this.radius + a.radius) / 2 - distance / 2),2));
-            return  Math.PI*Math.pow(Radius,2);
-        }
+
+        double Radius= Math.sqrt(
+                Math.pow(radius,2) - Math.pow(((double) (radius + a.radius) / 2 - distance / 2),2)
+        );
+        return Math.PI*Math.pow(Radius,2);
     }
 }

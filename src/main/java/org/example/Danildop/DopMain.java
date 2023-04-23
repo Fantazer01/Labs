acb391a106c07c2b9252ac88224e22871096f292
skorohodov97 <skorohodovdanil07@gmail.com>
2023-04-24 00:48:36 +0300

    Доп для 3 лабы

---
+++ src/main/java/org/example/Danildop/DopMain.java
@@ -0,0 +1,11 @@
+package org.example.Danildop;
+
+import org.example.lab3.task4_5.IntSequence;
+
+public class DopMain {
+        public static void main(String[] args) {
+
+        }
+
+
+}
---
+++ src/main/java/org/example/Danildop/MyIterator.java
@@ -0,0 +1,43 @@
+package org.example.Danildop;
+
+import java.util.Iterator;
+import java.util.function.Predicate;
+
+public class MyIterator<T> implements Iterator<T> {
+    private Iterator<T> iterator;
+    private Predicate<T> predicate=null;
+
+    private MyIterator(Iterator<T> iterator){
+        this.iterator=iterator;
+    }
+    private MyIterator(Iterator<T> iterator,Predicate<T> predicate){
+        this.iterator=iterator;
+        this.predicate=predicate;
+    }
+    public boolean hasNext() {
+
+        //если следующий элемент не подхдит по условию, смотрим следующий
+        //перебираем элементы пока не найдем подходящий предикату или пока не кончится
+        if(predicate==null)
+            return iterator.hasNext();
+        else {
+            Iterator<T> newiterator= iterator;
+            while(!predicate.test(newiterator.next())){
+                newiterator= (Iterator<T>) newiterator.next();
+            }
+            return newiterator.hasNext();
+        }
+    }
+
+    public T next() {
+        return iterator.next();
+    }
+
+    public static <T> MyIterator<T>  fromIterator(Iterator<T> iterator){
+        return new MyIterator<T>(iterator);
+    }
+    public MyIterator<T> filter(Predicate<T> predicate){
+        return  new MyIterator<T>(iterator,predicate);
+    }
+
+}

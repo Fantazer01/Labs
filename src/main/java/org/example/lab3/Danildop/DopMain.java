bdfff8e33f5dcaf9c3bfcb6eec3e7de325cfbf5b
Oleg <olegkrygin9@gmail.com>
2023-04-24 18:43:23 +0300

    Настроил функционал по 2ому пункту допа

--- src/main/java/org/example/lab3/Danildop/DopMain.java
+++ src/main/java/org/example/lab3/Danildop/DopMain.java
@@ -1,8 +1,33 @@
 package org.example.lab3.Danildop;
 
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.function.Predicate;
+
 public class DopMain {
         public static void main(String[] args) {
+            String[] array = getStrings();
 
+            Predicate<String> predicate = (str) -> {
+                return str.length() > 3;
+            };
+
+            Iterator<String> iterator = MyIterator.fromIterator(Arrays.stream(array)
+                                                .iterator()).filter(predicate);
+
+            while (iterator.hasNext()) {
+                System.out.println(iterator.next());
+            }
+
+        }
+
+        private static String[] getStrings() {
+            return new String[] {
+                "Aha",
+                "Ogo",
+                "Nice",
+                "Coca cola",
+            };
         }
 
 
--- src/main/java/org/example/lab3/Danildop/MyIterator.java
+++ src/main/java/org/example/lab3/Danildop/MyIterator.java
@@ -1,36 +1,45 @@
 package org.example.lab3.Danildop;
 
 import java.util.Iterator;
+import java.util.NoSuchElementException;
 import java.util.function.Predicate;
 
 public class MyIterator<T> implements Iterator<T> {
-    private Iterator<T> iterator;
-    private Predicate<T> predicate=null;
+    private final Iterator<T> iterator;
+    private Predicate<T> predicate = null;
+    private T item = null;
 
     private MyIterator(Iterator<T> iterator){
         this.iterator=iterator;
     }
-    private MyIterator(Iterator<T> iterator,Predicate<T> predicate){
-        this.iterator=iterator;
-        this.predicate=predicate;
+    private MyIterator(Iterator<T> iterator, Predicate<T> predicate){
+        this.iterator = iterator;
+        this.predicate = predicate;
+        this.item = null;
     }
     public boolean hasNext() {
-
         //если следующий элемент не подхдит по условию, смотрим следующий
         //перебираем элементы пока не найдем подходящий предикату или пока не кончится
         if(predicate==null)
             return iterator.hasNext();
-        else {
-            Iterator<T> newiterator= iterator;
-            while(!predicate.test(newiterator.next())){
-                newiterator= (Iterator<T>) newiterator.next();
-            }
-            return newiterator.hasNext();
+
+        while (iterator.hasNext()) {
+            item = iterator.next();
+            if (predicate.test(item))
+                return true;
         }
+
+        return false;
     }
 
     public T next() {
-        return iterator.next();
+        if (predicate == null)
+            return iterator.next();
+
+        if (item == null)
+            throw new NoSuchElementException();
+
+        return item;
     }
 
     public static <T> MyIterator<T>  fromIterator(Iterator<T> iterator){

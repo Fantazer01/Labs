f4ddfc54eef3237067377eb5a676b0db4bfd8c09
Oleg <olegkrygin9@gmail.com>
2023-04-24 22:03:54 +0300

    9 task + решенный доп. по enum

---
+++ src/main/java/org/example/lab4/Task9/Item.java
@@ -0,0 +1,11 @@
+package org.example.lab4.Task9;
+
+public class Item {
+
+    @Override
+    public String toString() {
+        Class<?> _class = Item.class;
+
+        return _class.getName();
+    }
+}
---
+++ src/main/java/org/example/lab4/Task9/Task9.java
@@ -0,0 +1,9 @@
+package org.example.lab4.Task9;
+
+public class Task9 {
+    public static void main(String[] args) {
+        Item item = new Item();
+
+        System.out.println(item);
+    }
+}

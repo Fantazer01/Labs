b8b24168c51ab470f8c5f826f3b6410b3f6f13bd
Oleg <olegkrygin9@gmail.com>
2023-04-24 20:07:01 +0300

    Добавил функционал по 3ому пункту допа

--- src/main/java/org/example/lab3/Danildop/DopMain.java
+++ src/main/java/org/example/lab3/Danildop/DopMain.java
@@ -1,27 +1,38 @@
 package org.example.lab3.Danildop;
 
+import org.example.lab3.comparator.Employee;
+
 import java.util.Arrays;
 import java.util.Iterator;
+import java.util.function.BiFunction;
 import java.util.function.Predicate;
 
 public class DopMain {
-        public static void main(String[] args) {
-            String[] array = getStrings();
+    public static void main(String[] args) {
+        System.out.println("---------------------------firstCase---------------------------");
+        firstCase();
+        System.out.println("---------------------------secondCase---------------------------");
+        secondCase();
+        System.out.println("---------------------------thirdCase---------------------------");
+        thirdCase();
+    }
 
-            Predicate<String> predicate = (str) -> {
-                return str.length() > 3;
-            };
+    private static void firstCase() {
+        String[] array = getStrings();
 
-            Iterator<String> iterator = MyIterator.fromIterator(Arrays.stream(array)
-                                                .iterator()).filter(predicate);
+        Predicate<String> predicate = (str) -> {
+            return str.length() > 3;
+        };
 
-            while (iterator.hasNext()) {
+        Iterator<String> iterator = MyIterator.fromIterator(Arrays.stream(array)
+                .iterator()).filter(predicate);
+
+        while (iterator.hasNext()) {
                 System.out.println(iterator.next());
-            }
-
         }
+    }
 
-        private static String[] getStrings() {
+    private static String[] getStrings() {
             return new String[] {
                 "Aha",
                 "Ogo",
@@ -30,5 +41,42 @@ public class DopMain {
             };
         }
 
+    private static void secondCase() {
+        Employee[] employees = getEmployees();
+
+        Predicate<Employee> predicate = (emp) -> {
+            return emp.getName().length() > 4;
+        };
+
+        Iterator<Employee> iterator = MyIterator.fromIterator(Arrays.stream(employees)
+                    .iterator()).filter(predicate);
+
+        while (iterator.hasNext())
+            System.out.println(iterator.next());
+    }
+
+    private static void thirdCase() {
+        Employee[] employees = getEmployees();
+
+        BiFunction<Employee, Employee, Employee> func =
+                (emp1, emp2) -> {
+                    return emp2;
+                };
+        Iterator<Employee> iterator = MyIterator.fromIterator(Arrays.stream(employees)
+                .iterator()).reduce(func);
+
+        while (iterator.hasNext())
+            System.out.println(iterator.next());
+    }
+
+    private static Employee[] getEmployees() {
+        return new Employee[] {
+                new Employee("Bioba", 393, false),
+                new Employee("Biba", 393, true),
+                new Employee("Biiba", 393, true),
+                new Employee("Boba", 939, false),
+                new Employee("Boss", -5, true)
+        };
+    }
 
 }
--- src/main/java/org/example/lab3/Danildop/MyIterator.java
+++ src/main/java/org/example/lab3/Danildop/MyIterator.java
@@ -2,51 +2,91 @@ package org.example.lab3.Danildop;
 
 import java.util.Iterator;
 import java.util.NoSuchElementException;
+import java.util.function.BiFunction;
 import java.util.function.Predicate;
 
 public class MyIterator<T> implements Iterator<T> {
-    private final Iterator<T> iterator;
-    private Predicate<T> predicate = null;
-    private T item = null;
+    private final Iterator<T> _iterator;
+    private Predicate<T> _predicate = null;
+    private T _item = null;
+    private T _result = null;
+    private BiFunction<T, T, T> _func = null;
 
     private MyIterator(Iterator<T> iterator){
-        this.iterator=iterator;
+        _iterator=iterator;
     }
     private MyIterator(Iterator<T> iterator, Predicate<T> predicate){
-        this.iterator = iterator;
-        this.predicate = predicate;
-        this.item = null;
+        _iterator = iterator;
+        _predicate = predicate;
     }
-    public boolean hasNext() {
-        //если следующий элемент не подхдит по условию, смотрим следующий
-        //перебираем элементы пока не найдем подходящий предикату или пока не кончится
-        if(predicate==null)
-            return iterator.hasNext();
+    private MyIterator(Iterator<T> iterator, BiFunction<T, T, T> func) {
+        _iterator = iterator;
+        _func = func;
+    }
 
-        while (iterator.hasNext()) {
-            item = iterator.next();
-            if (predicate.test(item))
+    public boolean hasNext() {
+        if (_predicate == null && _func == null)
+            return _iterator.hasNext();
+        if (_predicate != null)
+            return predicateHasNext();
+        return funcHasNext();
+    }
+
+    private boolean predicateHasNext() {
+        while (_iterator.hasNext()) {
+            _item = _iterator.next();
+            if (_predicate.test(_item))
                 return true;
         }
+        return false;
+    }
 
+    private boolean funcHasNext() {
+        if (_iterator.hasNext()) {
+            T item1 = _iterator.next();
+            if (_iterator.hasNext()) {
+                T item2 = _iterator.next();
+                _result = _func.apply(item1, item2);
+                return true;
+            }
+        }
         return false;
     }
 
     public T next() {
-        if (predicate == null)
-            return iterator.next();
+        if (_predicate == null && _func == null)
+            return _iterator.next();
 
-        if (item == null)
+        if (_predicate != null)
+            return predicateNext();
+
+        return funcNext();
+    }
+
+    private T predicateNext() {
+        if (_item == null)
             throw new NoSuchElementException();
-
+        T item = _item;
+        _item = null;
         return item;
+    }
+
+    private T funcNext() {
+        if (_result == null)
+            throw new NoSuchElementException();
+        T result = _result;
+        _result = null;
+        return result;
     }
 
     public static <T> MyIterator<T>  fromIterator(Iterator<T> iterator){
         return new MyIterator<T>(iterator);
     }
     public MyIterator<T> filter(Predicate<T> predicate){
-        return  new MyIterator<T>(iterator,predicate);
+        return  new MyIterator<T>(this, predicate);
+    }
+    public MyIterator<T> reduce(BiFunction<T, T, T> func) {
+        return new MyIterator<T>(this, func);
     }
 
 }
